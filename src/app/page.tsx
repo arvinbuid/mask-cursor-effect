222e75422f7bb28d9c9212c4d07ec93bff284f5b
Arvin Buid <arvinbuid01@gmail.com>
2025-06-03 16:25:17 +0800

    Implemented remove mask if screen size is below 768px breakpoint.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,43 @@
 import styles from './page.module.css';
 import useMousePosition from '../../hooks/useMousePosition';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 export default function Mask() {
   const [isHovered, setIsHovered] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(0); // Window width
+
   const { x, y } = useMousePosition();
+
+  useEffect(() => {
+    const initialWidth = () => setWindowWidth(window.innerWidth);
+
+    // Set initial width
+    initialWidth();
+
+    // Add event listener on window resize
+    window.addEventListener('resize', initialWidth);
+
+    // Remove event listener
+    return () => window.removeEventListener('resize', initialWidth);
+
+  }, [])
+
+  // If window width is less than 768px, hide the mask
+  if (windowWidth < 768 || x === null || y === null) {
+    return (
+      <main className={styles.main}>
+        <div className={styles.body}>
+          <p>Hello. <span>I&apos;m John Doe, </span>a web developer from Cavite, Philippines. I love to build things and provide business solutions.</p>
+        </div>
+      </main>
+    );
+  }
 
   const size = isHovered ? 300 : 40;
 
   if (x === null || y === null) return null;
+
 
   return (
     <main className={styles.main}>
